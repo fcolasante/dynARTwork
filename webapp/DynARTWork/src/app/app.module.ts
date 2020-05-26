81b9e09978490c8124b3b4655711171feeb3ccdd
Francesco Colasante <frank_997@hotmail.it>
2020-05-26 18:38:48 +0200

    added upload immagine

--- webapp/DynARTWork/src/app/app.module.ts
+++ webapp/DynARTWork/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule, BUCKET} from '@angular/fire/storage';
+import { AngularFireFunctionsModule, REGION, ORIGIN} from '@angular/fire/functions';
+
+import { environment } from '../environments/environment';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireFunctionsModule,
+    AppMaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: BUCKET, useValue: environment.firebase.storageBucket}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
