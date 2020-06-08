fe79a07bdd28b68b3662075cb03317a231fa1994
sditanna <ditanna.1765243@studenti.uniroma1.it>
2020-06-08 23:57:42 +0200

    showing image correctly

--- devices/stm32/notification_polling.py
+++ devices/stm32/notification_polling.py
@@ -47,80 +47,13 @@ below:
    bucket (you could use the console or gsutil) and watch as changes scroll by
    in the app.
 """
-
+import os
 import argparse
 import json
 import time
 import cv2
 from google.cloud import storage
 from google.cloud import pubsub_v1
-
-
-def summarize(message):
-    # [START parse_message]
-    data = message.data.decode("utf-8")
-    attributes = message.attributes
-
-    event_type = attributes["eventType"]
-    bucket_id = attributes["bucketId"]
-    object_id = attributes["objectId"]
-    generation = attributes["objectGeneration"]
-    description = (
-        "\tEvent type: {event_type}\n"
-        "\tBucket ID: {bucket_id}\n"
-        "\tObject ID: {object_id}\n"
-        "\tGeneration: {generation}\n"
-    ).format(
-        event_type=event_type,
-        bucket_id=bucket_id,
-        object_id=object_id,
-        generation=generation,
-    )
-
-    if "overwroteGeneration" in attributes:
-        description += "\tOverwrote generation: %s\n" % (
-            attributes["overwroteGeneration"]
-        )
-    if "overwrittenByGeneration" in attributes:
-        description += "\tOverwritten by generation: %s\n" % (
-            attributes["overwrittenByGeneration"]
-        )
-
-    payload_format = attributes["payloadFormat"]
-    if payload_format == "JSON_API_V1":
-        object_metadata = json.loads(data)
-        size = object_metadata["size"]
-        content_type = object_metadata["contentType"]
-        metageneration = object_metadata["metageneration"]
-        description += (
-            "\tContent type: {content_type}\n"
-            "\tSize: {object_size}\n"
-            "\tMetageneration: {metageneration}\n"
-        ).format(
-            content_type=content_type,
-            object_size=size,
-            metageneration=metageneration,
-        )
-    return description
-    # [END parse_message]
-
-
-def show_image(name):
-    storage_client = storage.Client.from_service_account_json('service_account.json')
-    # get bucket with name
-    bucket = storage_client.get_bucket('processed_artworks')
-    # get bucket data as blob
-
-    blob = bucket.get_blob(name)
-    # convert to stringfil
-    json_data = blob.download_as_string()
-
-    text_file = open(name, "wb")
-    n = text_file.write(json_data)
-    text_file.close()
-    img = cv2.imread('./{}'.format(name))
-    cv2.imshow(name, img)
-    #cv2.waitKey(10000)
 
 def poll_notifications(project, subscription_name):
     """Polls a Cloud Pub/Sub subscription for new GCS events for display."""
@@ -131,17 +64,54 @@ def poll_notifications(project, subscription_name):
     )
 
     def callback(message):
-        show_image(message.attributes['objectId'])
-        print("Received message:\n{}".format(summarize(message)))
-        message.ack()
+        #show_image(message.attributes['objectId'])
+        #print("Received message:\n{}".format(summarize(message)))
+        data = message.data.decode("utf-8")
+        attributes = message.attributes
 
+        event_type = attributes["eventType"]
+        bucket_id = attributes["bucketId"]
+        object_id = attributes["objectId"]
+        #generation = attributes["objectGeneration"]
+        var = "Message not important"
+        if "data" in object_id:
+            message.ack()
+        else:
+            storage_client = storage.Client.from_service_account_json('service_account.json')
+            # get bucket with name
+            bucket = storage_client.get_bucket('processed_artworks')
+            # get bucket data as blob
+            blob = bucket.get_blob(object_id)
+            # convert to stringfil
+            json_data = blob.download_as_string()
+            # open file in writing
+            text_file = open(object_id, "wb")
+            # write on file and close
+            n = text_file.write(json_data)
+            text_file.close()
+            # display the image
+            print("Showing image:{}".format(object_id))
+            img2 = cv2.imread('./{}'.format(object_id))
+            message.ack()
+            cv2.imshow("image", img2)
+            var = "Image correctly showed"
+        print("Result of callback:{}".format(var))
+        
+
+    #subscriber.subscribe(subscription_path, callback=callback)
     subscriber.subscribe(subscription_path, callback=callback)
-
     # The subscriber is non-blocking, so we must keep the main thread from
     # exiting to allow it to process messages in the background.
     print("Listening for messages on {}".format(subscription_path))
+    img = cv2.imread('./{}'.format("cristo.jpg"))
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    # for the full screen mode
+    #cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     while True:
-        time.sleep(60)
+        cv2.waitKey(0) 
+        #time.sleep(60)
+
+
     # [END poll_notifications]
 
 
